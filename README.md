99e8c5a215358488ba7e6028a297d0c9de4c8556
Christopher Posada <posada.chris@gmail.com>
2021-07-31 20:12:10 -0400

    Functional shortener working

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # lnkshrnk
-A url shortner 
+A url shortner
 
 
-[![wakatime](https://wakatime.com/badge/github/chrispydizzle/lnkshrnk.svg)](https://wakatime.com/@ae6967d3-6d3b-4433-834a-e44dc28e320f/projects/fymebeqvvd?start=2021-07-25&end=2021-07-31)
+
+### Time spent  
+[![wakatime](https://wakatime.com/badge/github/chrispydizzle/lnkshrnk.svg)](https://wakatime.com/@ae6967d3-6d3b-4433-834a-e44dc28e320f/projects/hcqhowizas?start=2021-07-31&end=2021-07-31)
+
+### More Project Information
+- [Project](https://github.com/chrispydizzle/lnkshrnk/projects/1)
+- [Wiki](https://github.com/chrispydizzle/lnkshrnk/wiki)
--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask
+import flask
+import smaz
+import base64
 
-app = Flask(__name__)
+
+app = flask.Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    content = 'https://dogeplanet.com'
+    compressed: bytes = smaz.compress(content)
+    b64_bytes: bytes = base64.b64encode(compressed)
+    short_url: str = b64_bytes.decode('ascii')
+    print(short_url)
+    b64_bytes_enc = short_url.encode('ascii')
+    b64decoded_compressed: bytes = base64.b64decode(b64_bytes_enc)
+    long_url: str = smaz.decompress(b64decoded_compressed)
+    origin_url: str = smaz.decompress(compressed)
+    print(long_url)
+    print(origin_url)
+
+
+@app.post('/shrink')
+def shrink():  # put application's code here
+    content = flask.request.json
+    compressed: bytes = smaz.compress(content['url'])
+    b64_bytes: bytes = base64.b64encode(compressed)
+    short_url: str = b64_bytes.decode('ascii')
+    print(short_url)
+    return flask.jsonify({"url": short_url})
+
+
+@app.post('/unshrink')
+def unshrink():  # put application's code here
+    content = flask.request.json
+    b64_bytes_enc = content['url'].encode('ascii')
+    b64decoded_compressed: bytes = base64.b64decode(b64_bytes_enc)
+    long_url: str = smaz.decompress(b64decoded_compressed)
+    return flask.jsonify({"url": long_url})
 
 
 if __name__ == '__main__':
